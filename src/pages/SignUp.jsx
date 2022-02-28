bdaa0b65ef952a80b88beb5c05f48a94470cac46
james-zedd <james_zedd@hotmail.com>
2022-02-28 08:46:59 -0500

    Completed lessons 84, 85

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,8 +1,79 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, createUserWithEmailAndPassword, updateProfile } from 'firebase/auth';
+import { db } from '../firebase.config';
+import { ReactComponent as ArrowRightIcon } from '../assets/svg/keyboardArrowRightIcon.svg';
+import visibilityIcon from '../assets/svg/visibilityIcon.svg';
+
 function SignUp() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+  });
+  const { name, email, password } = formData;
+
+  const navigate = useNavigate();
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      console.log(db);
+      const auth = getAuth();
+
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      });
+
+      navigate('/');
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
-    <div>
-      <h1>Sign Up</h1>
-    </div>
+    <>
+      <div className="pageContainer">
+        <header>
+          <p className="pageHeader">Welcome Back!</p>
+        </header>
+        <main>
+          <form onSubmit={onSubmit} >
+            <input type="text" className="nameInput" placeholder="name" id='name' value={name} onChange={onChange}  />
+            <input type="email" className="emailInput" placeholder="email" id='email' value={email} onChange={onChange}  />
+            <div className="passwordInputDiv">
+              <input type={showPassword ? 'text' : 'password'} className='passwordInput' placeholder='password' id='password' value={password} onChange={onChange} />
+              <img src={visibilityIcon} alt="show password" className='showPassword' onClick={() => setShowPassword((prevState) => !prevState)} />
+            </div>
+            <Link to='/forgot-password' className='forgotPasswordLink'>Forgot Password</Link>
+            <div className="signUpBar">
+              <p className="signUpText">Sign Up</p>
+              <button className="signUpButton">
+                <ArrowRightIcon fill='#fff' width='34px' height='34px' />
+              </button>
+            </div>
+          </form>
+
+          {/* Google oAuth */}
+
+          <Link to='/sign-in' className='registerLink'>
+            Sign in
+          </Link>
+        </main>
+      </div>
+    </>
   );
 }
 
